d72dd82ce20b3c2d928308700af5cae369130b7d
Pete Cheslock <petecheslock@gmail.com>
2024-01-12 15:56:55 -0500

    Create devcontainer for codespaces

---
+++ .devcontainer/postcreate.sh
@@ -0,0 +1,6 @@
+bundle install
+curl -fsSL https://packages.redis.io/gpg | sudo gpg --dearmor -o /usr/share/keyrings/redis-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/redis-archive-keyring.gpg] https://packages.redis.io/deb $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/redis.list
+bundle install --without production
+yarn install --check-files
+bundle exec rails db:migrate
