69bac6906417c8a9ab8acd4e510b60fb39e09ab6
Pete Cheslock <petecheslock@gmail.com>
2024-03-12 16:42:42 -0400

    Optimize followers page by addressing N+1 queries and inefficient data handling

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -50,14 +50,14 @@ class UsersController < ApplicationController
 
   def following
     @title = "Following"
-    @user  = User.find(params[:id])
+    @user  = User.includes(:following).find(params[:id])
     @users = @user.following.paginate(page: params[:page])
     render 'show_follow', status: :unprocessable_entity
   end
 
   def followers
     @title = "Followers"
-    @user  = User.find(params[:id])
+    @user  = User.includes(:followers).find(params[:id])
     @users = @user.followers.paginate(page: params[:page])
     render 'show_follow', status: :unprocessable_entity
   end
