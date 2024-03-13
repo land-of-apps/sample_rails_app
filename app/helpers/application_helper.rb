5cd1f9fc3f905b6e813565354cdb68a26608cee1
Pete Cheslock <petecheslock@gmail.com>
2024-03-13 17:07:29 -0400

    Implement and use a memoization pattern in the helpers for current_user

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,4 +9,9 @@ module ApplicationHelper
       "#{page_title} | #{base_title}"
     end
   end
+
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
 end
